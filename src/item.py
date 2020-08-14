ce39ff54c96442b26f94c83afe2ae8d0122bac9a
Eloy <eloydgutierrez@gmail.com>
2020-08-13 21:41:58 -0400

    added comments to item.py for my notes

--- src/item.py
+++ src/item.py
@@ -4,7 +4,10 @@ class Item:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-    
+    '''
+    def __str__(self): # NOT USED IN CODE JUST FOR REFERENCE
+        return f"{self.name}: {self.description}"
+    '''
     def on_take(self):
         print(f"\nYou have picked up {self.name}")
     
